f6fcb435e6ecec6b216898a7ae88f47366e7764b
sahilkaushik93 <kaushiksahil1412@gmail.com>
2025-03-17 13:19:51 +0530

    revising R & Python together

---
+++ R-Practise/practise.r
@@ -0,0 +1,14 @@
+
+
+
+# Hello in R
+print("hello")
+
+# Hello in python in .R script
+# install.packages("reticulate")
+library(reticulate)
+str = 'print("Hello R using python.")'
+py_run_string(str)
+
+
+
